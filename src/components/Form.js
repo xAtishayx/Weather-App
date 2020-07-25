6005355e977a16252fd80b633d90179d9c7eef80
Atishay <xatishayx@pop-os.localdomain>
2020-07-25 13:36:08 +0530

    fixed minor issue 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 export default class Form extends Component {
   render() {
     return (
-      <div className="bg" >
-        
-      <div className="form-container" style={this.props.data.req ? { marginTop:"-42vh" } : { backgroundImage:"linear-gradient( 150deg, rgba(22, 22, 24, 0.801) 25%, #221318c9 70%, #2c1010d8 84% )"}} >
+      <div className="bg" style={{backgroundImage:"linear-gradient( 150deg, rgba(22, 22, 24, 0.801) 25%, #221318c9 70%, #2c1010d8 84% )" }}>
+        {/* //backgroundImage:"linear-gradient( 150deg, rgba(22, 22, 24, 0.801) 25%, #221318c9 70%, #2c1010d8 84% )" */}
+      <div className="form-container" style={this.props.data.req ? { marginTop:"-42vh" } : { }} >
      
         <form onSubmit={this.props.getWeather}>
           <input
@@ -20,7 +20,7 @@ export default class Form extends Component {
             id="exampleInputCountry1"
             placeholder="ENTER COUNTRY"
           />
-          <button type="submit" className="btn btn-outline-info">
+          <button type="submit" className="btn btn-outline-info" height="100" width="100">
             GET WEATHER
           </button>
         </form>
